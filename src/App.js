4855632a68faac50c594404acb51bde0dd7d4b05
Mark Fleckser <markfleckser@wireless-10-105-217-123.umd.edu>
2024-05-06 18:04:12 -0400

    Added petition text

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
 import NavBar from "./components/NavBar";
+import Petition from './pages/petition/Petition';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Router>
-          <NavBar />
+        <div id="nav"><NavBar /></div>
+        <div id="content">
           <Routes>
-            <Route path="/" element={<></>} />
+            <Route path="/" element={<Petition />} />
             <Route path="/read" element={<></>} />
             <Route path="/motivation" element={<></>} />
           </Routes>
+        </div>
       </Router>
     </div>
   );
---
+++ src/pages/petition/Petition.js
@@ -0,0 +1,31 @@
+import "./Petition.css";
+import { useState } from "react";
+
+function Petition() {
+    const [value, setValue] = useState("");
+
+    const updateValue = e => {
+        setValue(e.target.value);
+    }
+
+    const submitForm = () => {
+        setValue("");
+    }
+
+    return (<div id="petition">
+        <p id="petition-text">
+            <pre>   </pre>In the novel 1984, George Orwell envisions a world devoid of privacy and individuality. In the story, the population is constantly monitored and is held in check by the fear of intense punishment whenever they even slightly step out of line. In the real world, it would be difficult for any party or organization to reach this power level without facing severe resistance from citizens. However, if an organization were to watch people without their knowledge or consent, they could potentially gain Orwellian levels of power unbeknownst to the general population. In a world where technologies are created every day and where companies can learn more about your life than your own family members, it is more important than ever that we stand up for our fundamental human right to privacy.<br /><br />
+            <pre>   </pre>At the beginning of 2024, Reddit announced that it was selling access to its collection of user-generated data to several large artificial intelligence companies, including Google (Dave). This transaction is just one instance of data’s transformation into a deadly weapon companies pay hundreds of millions for. As companies become more and more capable of leveraging Orwellian powers, the urgency for regulations on the collection and usage of data grows equally. Even back in 2012, public panic erupted in response to a Forbes article claiming that Target had developed an algorithm that could accurately predict if a customer was pregnant (Hill). A decade ago, companies were already starting to identify extremely intimate details in peoples’ lives, such as pregnancy, to deliver customized advertisements and coupons to their targets. Imagine what Target knows about you now.<br /><br />
+            <pre>   </pre>In the United States, there are currently no detailed federal data privacy laws and only a few states are just beginning to develop regulations controlling what companies can do with user data (Halladay). Hundreds of millions of consumers are powerless against corporate manipulation through their data. It is crucial to establish regulations that protect consumers of digital products.<br /><br />
+            <pre>   </pre>A complete digital data privacy law would have to address at minimum direct data collection, data surveillance, data sharing, and data usage. The specific content of the law would define limits to how companies can collect, use, and share data in different situations. It would also discuss the interactions between firms and consumers, and establish requirements for businesses to communicate their methods, purposes, and policies relating to users’ data.<br /><br />
+            <pre>   </pre>We must decide upon an appropriate balance of power between technology companies and their customers. We are entering a new era of technology with thrilling possibilities for novel developments, but the threat of complete corporate surveillance and control also looms. If we don’t act with urgency, we risk losing what makes us human: the ability to choose a life for ourselves.<br /><br />
+            <pre>   </pre>Sign to the right to save humanity from sacrificing itself for the sake of technology.</p>
+        <div id="petition-signing">
+            <h1>Sign Here</h1>
+            <input id="signature-input" value={value} onChange={updateValue} type="text" placeholder="Your Name Here" />
+            <button id="signature-submit" onClick={submitForm}>Sign</button>
+        </div>
+    </div>);
+}
+
+export default Petition;
